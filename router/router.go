006e858865c6f0cdfcd1911a9cd5d6dc469725af
chuxin <chuxin0816@qq.com>
2023-10-11 00:25:29 +0800

    初始化项目

--- router/router.go
+++ router/router.go
@@ -24,25 +24,25 @@ func Setup(conf *config.HertzConfig) *server.Hertz {
 
 	// basic apis
 	apiRouter.GET("/feed/", controller.Feed)
-	apiRouter.GET("/user/", controller.UserInfo)
-	apiRouter.POST("/user/register/", controller.Register)
-	apiRouter.POST("/user/login/", controller.Login)
-	apiRouter.POST("/publish/action/", controller.Publish)
-	apiRouter.GET("/publish/list/", controller.PublishList)
+	// apiRouter.GET("/user/", controller.UserInfo)
+	// apiRouter.POST("/user/register/", controller.Register)
+	// apiRouter.POST("/user/login/", controller.Login)
+	// apiRouter.POST("/publish/action/", controller.Publish)
+	// apiRouter.GET("/publish/list/", controller.PublishList)
 
-	// extra apis - I
-	apiRouter.POST("/favorite/action/", controller.FavoriteAction)
-	apiRouter.GET("/favorite/list/", controller.FavoriteList)
-	apiRouter.POST("/comment/action/", controller.CommentAction)
-	apiRouter.GET("/comment/list/", controller.CommentList)
+	// interaction apis
+	// apiRouter.POST("/favorite/action/", controller.FavoriteAction)
+	// apiRouter.GET("/favorite/list/", controller.FavoriteList)
+	// apiRouter.POST("/comment/action/", controller.CommentAction)
+	// apiRouter.GET("/comment/list/", controller.CommentList)
 
-	// extra apis - II
-	apiRouter.POST("/relation/action/", controller.RelationAction)
-	apiRouter.GET("/relation/follow/list/", controller.FollowList)
-	apiRouter.GET("/relation/follower/list/", controller.FollowerList)
-	apiRouter.GET("/relation/friend/list/", controller.FriendList)
-	apiRouter.GET("/message/chat/", controller.MessageChat)
-	apiRouter.POST("/message/action/", controller.MessageAction)
+	// social apis
+	// apiRouter.POST("/relation/action/", controller.RelationAction)
+	// apiRouter.GET("/relation/follow/list/", controller.FollowList)
+	// apiRouter.GET("/relation/follower/list/", controller.FollowerList)
+	// apiRouter.GET("/relation/friend/list/", controller.FriendList)
+	// apiRouter.GET("/message/chat/", controller.MessageChat)
+	// apiRouter.POST("/message/action/", controller.MessageAction)
 
 	return h
 }
