4c6e2cb7dcec5e4a95a433ed248bf1c97352107a
chuxin <chuxin0816@qq.com>
2023-10-11 00:23:01 +0800

    初始化项目

--- router/router.go
+++ router/router.go
@@ -20,5 +20,29 @@ func Setup(conf *config.HertzConfig) *server.Hertz {
 		ctx.JSON(consts.StatusOK, utils.H{"message": "pong"})
 	})
 
+	apiRouter := h.Group("/douyin")
+
+	// basic apis
+	apiRouter.GET("/feed/", controller.Feed)
+	apiRouter.GET("/user/", controller.UserInfo)
+	apiRouter.POST("/user/register/", controller.Register)
+	apiRouter.POST("/user/login/", controller.Login)
+	apiRouter.POST("/publish/action/", controller.Publish)
+	apiRouter.GET("/publish/list/", controller.PublishList)
+
+	// extra apis - I
+	apiRouter.POST("/favorite/action/", controller.FavoriteAction)
+	apiRouter.GET("/favorite/list/", controller.FavoriteList)
+	apiRouter.POST("/comment/action/", controller.CommentAction)
+	apiRouter.GET("/comment/list/", controller.CommentList)
+
+	// extra apis - II
+	apiRouter.POST("/relation/action/", controller.RelationAction)
+	apiRouter.GET("/relation/follow/list/", controller.FollowList)
+	apiRouter.GET("/relation/follower/list/", controller.FollowerList)
+	apiRouter.GET("/relation/friend/list/", controller.FriendList)
+	apiRouter.GET("/message/chat/", controller.MessageChat)
+	apiRouter.POST("/message/action/", controller.MessageAction)
+
 	return h
 }
