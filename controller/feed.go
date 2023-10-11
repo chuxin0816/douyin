d323b88b02113e73bc34eb976294a607f552964c
chuxin <chuxin0816@qq.com>
2023-10-12 01:20:48 +0800

    结束调试

--- controller/feed.go
+++ controller/feed.go
@@ -31,26 +31,6 @@ func Feed(c context.Context, ctx *app.RequestContext) {
 	response.Success(ctx, response.FeedResponse{
 		Response:  &response.Response{StatusCode: response.CodeSuccess, StatusMsg: resp.StatusCode.Msg()},
 		NextTime:  resp.NextTime,
-		VideoList: DemoVideos,
+		VideoList: resp.VideoList,
 	})
-}
-
-var DemoVideos = []*response.VideoResponse{
-	{
-		ID:            1,
-		Author:        &DemoUser,
-		PlayURL:       "https://www.w3schools.com/html/movie.mp4",
-		CoverURL:      "https://cdn.pixabay.com/photo/2016/03/27/18/10/bear-1283347_1280.jpg",
-		FavoriteCount: 0,
-		CommentCount:  0,
-		IsFavorite:    false,
-	},
-}
-
-var DemoUser = response.UserResponse{
-	ID:            1,
-	Name:          "TestUser",
-	FollowCount:   0,
-	FollowerCount: 0,
-	IsFollow:      false,
 }
