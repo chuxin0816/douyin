30bbaf0f11d885a168f04362e27086bc59b6f31f
chuxin <chuxin0816@qq.com>
2023-10-29 11:31:48 +0800

    创建于message相关的结构体并格式化其他结构体

---
+++ models/message.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type Message struct {
+	ID         int64     `json:"id" gorm:"primaryKey"`                       // 消息id
+	ToUserID   int64     `json:"to_user_id" gorm:"index:idx_to_user_id"`     // 消息接收者id
+	FromUserID int64     `json:"from_user_id" gorm:"index:idx_from_user_id"` // 消息发送者id
+	Content    string    `json:"content"`                                    // 消息内容
+	CreateTime time.Time `json:"create_time"`                                // 消息发送时间 
+}
