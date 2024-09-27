5b0ee6d7f18fc421368c616b19974b3aa521ae78
chuxin <im.chuxin@gmail.com>
2024-09-28 00:31:24 +0800

    聊天记录使用MySQL存储

--- etc/message.js
+++
@@ -1,9 +0,0 @@
-douyin = db.getSiblingDB('douyin');
-douyin.getCollection("message").drop();
-douyin.createCollection("message");
-douyin.getCollection("message").createIndex({
-    convert_id: NumberInt("1"),
-    create_time: NumberInt("1")
-}, {
-    name: "idx_convertId_createTime"
-});
